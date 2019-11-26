786751401d6644471d6fd5b13b4709fe3bf6939c
Marques Wright <wright.marques@gmail.com>
2019-11-26 11:24:29 -0500

    Update test_phoneselection.py
    Added the comment that includes the names of the people that will contribute to this program.

--- test_phoneselection.py
+++ test_phoneselection.py
@@ -1,2 +1,8 @@
+#----------------------------------------------
+# ITEC 3250 Software Engineering
+# Automated Test Code
+# William Wiggins, Nasir Martin, Marques Wright
+#----------------------------------------------
+
 import unittest
 import datetime
