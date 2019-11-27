bf4ce864ae3090da657ae1f3dfbf931dda277e39
Marques Wright <wright.marques@gmail.com>
2019-11-26 23:38:16 -0500

    Update test_phoneselection.py
    Added 3 test functions that:
    * checks to see if the customer is buying a phone
    * checks to see if the customer already owns their phone
    * checks to see if the number of lines the customer is requesting falls within range

--- test_phoneselection.py
+++ test_phoneselection.py
@@ -6,3 +6,24 @@
 
 import unittest
 import datetime
+import phoneselection
+
+# Defining a class that will test different cases for the 
+# different functions with the program
+class TestPhoneSelection(unittest.TestCase):
+
+    def test_inhouse(self):
+        """This function checks if the customer is buying a phone"""
+        choice = phoneselection.inhouse()
+        self.assertIs(choice, 0)
+
+    def test_owns(self):
+        """This function checks if the customer owns their phone"""
+        choice = phoneselection.owns()
+        self.assertIs(choice, 1)
+
+    def test_numOlines(self):
+        """This function checks for how many lines a customer is requesting"""
+        result = phoneselection.numOlines()
+        self.assertGreater(result, 0)
+        self.assertLess(result, 9)
