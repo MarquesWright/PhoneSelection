daaeeb038b86bdb766a14af3d41cd9018dca8313
nman247 <57647876+nman247@users.noreply.github.com>
2019-11-27 10:25:15 -0500

    Update phoneselection.py

--- phoneselection.py
+++ phoneselection.py
@@ -1,1 +1,40 @@
+import math
+cricketpho = float(input("What is the current price of monthly service at cricket?"))
+verizonpho = float(input("What is the current price of monthly service at verizon?"))
+sprintpho  = float(input("What is the current price of monthly service at sprint?"))
+ans = str(input("Are you bring over your own phone?"))
+if ans == "No" and "no":
+    phoprice = float(input("What is the cost of the phone you will be using?"))
+else:
+    phoprice = 0
+def feature():
+    Hotspot = str(input("Do you want to include Hotspot? "))
+    if Hotspot == "Yes":
+        crickethotprice = float(input("What is the cost of cricket Hotspot feature? "))
+        verizonhotprice = float(input("What is the cost of verizon Hotspot feature? "))
+        sprinthotprice = float(input("What is the cost of sprint Hotspot feature? "))
+        crickettotal = crickethotprice + phoprice + cricketpho
+        verizontotal = verizonhotprice + phoprice + verizonpho
+        sprinttotal  = sprinthotprice + phoprice + sprintpho
+        if crickettotal <= verizontotal and crickettotal <= sprinttotal:
+                print("Cricket has the lowest total cost with the low total price of " + str(crickettotal))
+        if verizontotal <= crickettotal and verizontotal <= sprinttotal:
+                print("Verizon has the lowest total cost with the low total price of " + str(verizontotal))
+        if sprinttotal <= crickettotal and sprinttotal <= verizontotal:
+                print("Sprint has the lowest total cost with the low total price of " + str(sprinttotal))
+    else:
+        crickethotprice = 0
+        verizonhotprice = 0
+        sprinthotprice = 0
+        crickettotal= crickethotprice + phoprice + cricketpho
+        verizontotal= verizonhotprice + phoprice + verizonpho
+        sprinttotal = sprinthotprice + phoprice + sprintpho
+        if crickettotal <= verizontotal and crickettotal <= sprinttotal:
+                print("Cricket has the lowest total cost with the low total price of " + str(crickettotal))
+        if verizontotal <= crickettotal and verizontotal <= sprinttotal:
+                print("Verizon has the lowest total cost with the low total price of " + str(verizontotal))
+        if sprinttotal <= crickettotal and sprinttotal <= verizontotal:
+                print("Sprint has the lowest total cost with the low total price of " + str(sprinttotal))
+                
+feature()
 
