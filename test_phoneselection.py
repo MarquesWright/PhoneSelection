8181b0650492409d4b464698481d52ab9116eee8
Marques Wright <wright.marques@gmail.com>
2019-12-02 13:45:39 -0500

    Update test_phoneselection.py
    I added a few more blank classes and functions that will be used in conjunction with the existing code to test the program. I will be adding more code to the overall structure of the file once I have fully understood the direction to proceed in.

--- test_phoneselection.py
+++ test_phoneselection.py
@@ -27,3 +27,29 @@ class TestPhoneSelection(unittest.TestCase):
         result = phoneselection.numOlines()
         self.assertGreater(result, 0)
         self.assertLess(result, 9)
+
+class TestHandlingImproperCases(unittest.TestCase):
+
+    def test_correctnumberformat(self):
+        """Test to determine if inputs are in the required numeral format"""
+
+
+    def test_validrange(self):
+
+
+    def test_costoffeatures(self):
+
+
+class TestTimeToAnswer(unittest.TestCase):
+
+    def test_timegivenforresults(self):
+
+
+class TestReliability(unittest.TestCase):
+
+    def test_matchingprices(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
