6d5f5625c5a785ba4fc36f963b8d2b3ff7883c31
Marques Wright <wright.marques@gmail.com>
2019-12-03 23:44:19 -0500

    Update test_phoneselection.py

--- test_phoneselection.py
+++ test_phoneselection.py
@@ -14,12 +14,14 @@ class TestPhoneSelection(unittest.TestCase):
     #               or if they are buying a phone
     def test_ownorbuying(self):
         # Capture the results of the function
-        result = phoneselection.ownorbuying()
+        result1 = phoneselection.ownorbuying('Own')
+        result2 = phoneselection.ownorbuying('Buy')
+        #result2 = phoneselection.ownorbuying('Buy')
         # Check for expected ouput
         owns = 0
         buying = 1
-        self.assertEqual(owns, result, "Customer owns the phone")
-        self.assertEqual(buying, result, "Customer is buying a new phone")
+        self.assertEqual(owns, result1, msg="Customer owns the phone")
+        self.assertEqual(buying, result2, msg="Customer is buying a new phone")
 
         # Psuedocode for the Boolean combining the above functions
         """ Psuedocode Boolean combining above processes
@@ -33,15 +35,46 @@ class TestPhoneSelection(unittest.TestCase):
         
     # Test Query 2: Test numoflines() to see if the value falls
     #               within a valid range
-    def test_numOlines(self):
-        result = phoneselection.numOlines()
-        self.assertGreater(result, 0)
-        self.assertLess(result, 9)
+    # Test Query 2: Test numoflines() to see if the value falls
+    #               within a valid range
+    def test_numOflines(self):
+        # Capture the results from the user input
+        result1 = phoneselection.numOflines(1)
+        result2 = phoneselection.numOflines(2)
+        result3 = phoneselection.numOflines(3)
+        result4 = phoneselection.numOflines(4)
+        result5 = phoneselection.numOflines(5)
+        result6 = phoneselection.numOflines(6)
+        result7 = phoneselection.numOflines(7)
+        result8 = phoneselection.numOflines(8)
+        # Check for expected output
+        """Checking to see if the result is greater than 0"""
+        self.assertGreater(result1, 0)
+        self.assertGreater(result2, 0)
+        self.assertGreater(result3, 0)
+        self.assertGreater(result4, 0)
+        self.assertGreater(result5, 0)
+        self.assertGreater(result6, 0)
+        self.assertGreater(result7, 0)
+        self.assertGreater(result8, 0)
+        """Checking to see if the result is less than 9"""
+        self.assertLess(result1, 9)
+        self.assertLess(result2, 9)
+        self.assertLess(result3, 9)
+        self.assertLess(result4, 9)
+        self.assertLess(result5, 9)
+        self.assertLess(result6, 9)
+        self.assertLess(result7, 9)
+        self.assertLess(result8, 9)
         
     # Test Query 3: Test listofvendors() for the correct spelling
     def test_phonetype(self):
-        style = phoneselection.phonetype()
-        self.assertEqual()
+        style1 = phoneselection.phonetype('Smartphone')
+        style2 = phoneselection.phonetype('Basic')
+        smart = 'Smartphone'
+        base = 'Basic'
+        self.assertEqual(style1, smart, msg="Customer chose a smartphone")
+        self.assertEqual(style2, base, msg="Customer chose a basic phone")
         
     # Test Query 4: Test
     def test_features(self):
