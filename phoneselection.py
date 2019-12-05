b2443577f3cc3e0eb03383b17e0fe90411261442
nman247 <57647876+nman247@users.noreply.github.com>
2019-12-04 19:10:56 -0500

    Update phoneselection.py

--- phoneselection.py
+++ phoneselection.py
@@ -1,19 +1,70 @@
-cricketpho = float(input("What is the current price of monthly service at cricket?"))
-verizonpho = float(input("What is the current price of monthly service at verizon?"))
-sprintpho  = float(input("What is the current price of monthly service at sprint?"))
 ans = str(input("Are you bring over your own phone?"))
 gbans = int(input("How many gigabytes are you planning to use?"))
-def phoneprice():
-    if ans == "Yes" or ans == "yes":
-        phoprice = float(input("What is the cost of the phone you will be using?"))
-    else:
-        phoprice = 0
+numofpho = int(input("How many lines are you planning to add? "))
+if gbans >= 5 and numofpho == 4:
+    cricketpho = 100
+    verizonpho = 140
+    sprintpho = 100
+elif gbans > 5 and numofpho >= 4:
+    cricketpho = 25 * numofpho
+    verizonpho = 35 * numofpho
+    sprintpho = 100
+elif gbans < 5 and gbans >= 2  and numofpho == 4:
+    cricketpho = 90
+    verizonpho = 140
+    sprintpho = 100
+elif gbans < 2 and numofpho == 4:
+    cricketpho = 100
+    verizonpho = 140
+    sprintpho = 100
+elif gbans >= 5 and numofpho == 3:
+    cricketpho = 90
+    verizonpho = 135
+    sprintpho = 100
+elif gbans < 5 and gbans >= 2 and numofpho == 3:
+    cricketpho = 90
+    verizonpho = 135
+    sprintpho = 100
+elif gbans < 2 and numofpho == 3:
+    cricketpho = 90
+    verizonpho = 135
+    sprintpho = 100
+elif gbans >= 5 and numofpho == 2:
+    cricketpho = 90
+    verizonpho = 120
+    sprintpho = 100
+elif gbans < 5 and gbans >= 2 and numofpho == 2:
+    cricketpho = 80
+    verizonpho = 120
+    sprintpho = 100
+elif gbans < 2 and numofpho == 2:
+    cricketpho = 75
+    verizonpho = 120
+    sprintpho = 100 
+elif gbans > 5 and numofpho == 1:
+    cricketpho = 55
+    verizonpho = 60
+    sprintpho = 70
+elif gbans < 5 and gbans >= 2 and numofpho == 1:
+    cricketpho = 40
+    verizonpho = 70
+    sprintpho = 60
+elif gbans < 2 and numofpho == 1:
+    cricketpho = 30
+    verizonpho = 70
+    sprintpho = 60 
+if ans == "Yes" or ans == "yes":
+    phoprice = float(input("What is the cost of the phone you will be using?"))
+    
+else:
+    phoprice = 0
+    
 def feature():
     Hotspot = str(input("Do you want to include Hotspot? "))
     if Hotspot == "Yes" or Hotspot == "yes":
-        crickethotprice = float(input("What is the cost of cricket Hotspot feature? "))
-        verizonhotprice = float(input("What is the cost of verizon Hotspot feature? "))
-        sprinthotprice = float(input("What is the cost of sprint Hotspot feature? "))
+        crickethotprice = 10 * numofpho
+        verizonhotprice = 10 * numofpho
+        sprinthotprice = 0
         crickettotal = crickethotprice + phoprice + cricketpho
         verizontotal = verizonhotprice + phoprice + verizonpho
         sprinttotal  = sprinthotprice + phoprice + sprintpho
@@ -36,7 +87,8 @@ def feature():
                 print("Verizon has the lowest total cost with the low total price of " + str(verizontotal))
         if sprinttotal <= crickettotal and sprinttotal <= verizontotal:
                 print("Sprint has the lowest total cost with the low total price of " + str(sprinttotal))
-phoneprice()
+
+
 feature()
 
 
