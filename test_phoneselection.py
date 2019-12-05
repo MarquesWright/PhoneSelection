eb9ea33efe85f8e67b8f0e2511c22bc76bec2a8f
Marques Wright <wright.marques@gmail.com>
2019-12-04 22:25:23 -0500

    Update test_phoneselection.py

--- test_phoneselection.py
+++ test_phoneselection.py
@@ -12,15 +12,12 @@ class TestPhoneSelection(unittest.TestCase):
     
     # Test Query 1: Test ownorbuying() for whether they own a phone
     #               or if they are buying a phone
-    def test_ownorbuying(self):
-        # Capture the results of the function
-        result1 = phoneselection.ownorbuying('Buy')
-        result2 = phoneselection.ownorbuying('Own')
-        # Check for expected ouput
-        buying = 0
-        owns = 1
-        self.assertEqual(buying, result1, msg="Customer is buying a new phone")
-        self.assertEqual(owns, result2, msg="Customer owns the phone")
+    def test_ownorbuying(self, phone):
+        self.phone = phone
+        own = 0
+        buy = 1
+        self.assertEqual(phoneselection.ownorbuying(self.phone), own)
+        self.assertEqual(phoneselection.ownorbuying(self.phone), buy)
 
         # Psuedocode for the Boolean combining the above functions
         """ Psuedocode Boolean combining above processes
@@ -34,58 +31,32 @@ class TestPhoneSelection(unittest.TestCase):
         
     # Test Query 2: Test numoflines() to see if the value falls
     #               within a valid range
-    # Test Query 2: Test numoflines() to see if the value falls
-    #               within a valid range
-    def test_numOflines(self):
-        # Capture the results from the user input
-        result1 = phoneselection.numOflines(1)
-        result2 = phoneselection.numOflines(2)
-        result3 = phoneselection.numOflines(3)
-        result4 = phoneselection.numOflines(4)
-        result5 = phoneselection.numOflines(5)
-        result6 = phoneselection.numOflines(6)
-        result7 = phoneselection.numOflines(7)
-        result8 = phoneselection.numOflines(8)
-        # Check for expected output
-        """Checking to see if the result is greater than 0"""
-        self.assertGreater(result1, 0)
-        self.assertGreater(result2, 0)
-        self.assertGreater(result3, 0)
-        self.assertGreater(result4, 0)
-        self.assertGreater(result5, 0)
-        self.assertGreater(result6, 0)
-        self.assertGreater(result7, 0)
-        self.assertGreater(result8, 0)
-        """Checking to see if the result is less than 9"""
-        self.assertLess(result1, 9)
-        self.assertLess(result2, 9)
-        self.assertLess(result3, 9)
-        self.assertLess(result4, 9)
-        self.assertLess(result5, 9)
-        self.assertLess(result6, 9)
-        self.assertLess(result7, 9)
-        self.assertLess(result8, 9)
+    def test_numOflines(self, num):
+        self.num = num
+
+        self.assertGreaterEqual(phoneselection.numOflines(self.num), 1)
+        self.assertLessEqual(phoneselection.numOflines(self.num), 8)
         
     # Test Query 3: Test phonetype() for the correct spelling
     #               of the phone type
-    def test_phonetype(self):
-        style1 = phoneselection.phonetype('Smartphone')
-        style2 = phoneselection.phonetype('Basic')
+    def test_phonetype(self, typeOfphone):
+        self.typeOfphone = typeOfphone
         smart = 'Smartphone'
         base = 'Basic'
-        self.assertEqual(style1, smart, msg="Customer chose a smartphone")
-        self.assertEqual(style2, base, msg="Customer chose a basic phone")
+        self.assertEqual(phoneselection.phonetype(self.typeOfphone), smart)
+        self.assertEqual(phoneselection.phonetype(self.typeOfphone), base)
         
     # Test Query 4: Test features() for incompatible features
-    def test_features(self):
+    def test_feature(self, feature):
+        self.feature = feature
         feature1 = 'International Calling'
-        feature2 = 'HotSpot'
+        feature2 = 'Hotspot'
         feature3 = 'Metered Data'
         feature4 = 'Unlimited Data'
-        self.assertEqual(feature1, phoneselection.features('International Calling'))
-        self.assertEqual(feature2, phoneselection.features('HotSpot'))
-        self.assertEqual(feature3, phoneselection.features('Metered Data'))
-        self.assertEqual(feature4, phoneselection.features('Unlimited Data'))
+        self.assertEqual(phoneselection.feature(self.feature), feature1)
+        self.assertEqual(phoneselection.feature(self.feature), feature2)
+        self.assertEqual(phoneselection.feature(self.feature), feature3)
+        self.assertEqual(phoneselection.feature(self.feature), feature4)
 
 class TestHandlingImproperCases(unittest.TestCase):
 
