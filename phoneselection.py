22cc9f8ce256750ed828b99cb4fec122d0cd522a
Marques Wright <wright.marques@gmail.com>
2022-10-08 17:47:21 -0400

    Changed around the argument in the test method.

--- phoneselection.py
+++ phoneselection.py
@@ -1,1 +1,15 @@
+class PhoneSelection(object):
+    # create a method to ask if user owns phone 
+    # or will be buying a new one
+    def ownorbuying(self, answer):
+        if answer == 'Own' or answer == 'own':
+            return 0
+        elif answer == 'Buy' or answer == 'buy':
+            return 1
+        #else:
+            #raise ("Invalid ")
 
+if __name__ == '__main__':
+    phone = PhoneSelection()
+    #answer1 = input("Do You Own A Phone or Buying a New One? Answer Own or Buy: ")
+    response1 = phone.ownorbuying("Own")
--- test_phoneselection.py
+++ test_phoneselection.py
@@ -5,19 +5,22 @@
 #----------------------------------------------
 
 import unittest
-import phoneselection
+from phoneselection import PhoneSelection
 
 
 class TestPhoneSelection(unittest.TestCase):
+
+    # Create the setUp to intitialize object
+    def setUp(self):
+        self.phoneT = PhoneSelection()
     
     # Test Query 1: Test ownorbuying() for whether they own a phone
     #               or if they are buying a phone
-    def test_ownorbuying(self, phone):
-        self.phone = phone
+    def test_ownorbuying(self):
         own = 0
         buy = 1
-        self.assertEqual(phoneselection.ownorbuying(self.phone), own)
-        self.assertEqual(phoneselection.ownorbuying(self.phone), buy)
+        self.assertEqual(own, self.phoneT.ownorbuying())
+        self.assertEqual(buy, self.phoneT.ownorbuying(self.phone))
 
         # Psuedocode for the Boolean combining the above functions
         """ Psuedocode Boolean combining above processes
@@ -31,58 +34,58 @@ class TestPhoneSelection(unittest.TestCase):
         
     # Test Query 2: Test numoflines() to see if the value falls
     #               within a valid range
-    def test_numOflines(self, num):
-        self.num = num
+    #def test_numOflines(self, num):
+        #self.num = num
 
-        self.assertGreaterEqual(phoneselection.numOflines(self.num), 1)
-        self.assertLessEqual(phoneselection.numOflines(self.num), 8)
+        #self.assertGreaterEqual(phoneselection.numOflines(self.num), 1)
+        #self.assertLessEqual(phoneselection.numOflines(self.num), 8)
         
     # Test Query 3: Test phonetype() for the correct spelling
     #               of the phone type
-    def test_phonetype(self, typeOfphone):
-        self.typeOfphone = typeOfphone
-        smart = 'Smartphone'
-        base = 'Basic'
-        self.assertEqual(phoneselection.phonetype(self.typeOfphone), smart)
-        self.assertEqual(phoneselection.phonetype(self.typeOfphone), base)
+    #def test_phonetype(self, typeOfphone):
+        #self.typeOfphone = typeOfphone
+        #smart = 'Smartphone'
+        #base = 'Basic'
+        #self.assertEqual(phoneselection.phonetype(self.typeOfphone), smart)
+        #self.assertEqual(phoneselection.phonetype(self.typeOfphone), base)
         
     # Test Query 4: Test features() for incompatible features
-    def test_feature(self, feature):
-        self.feature = feature
-        feature1 = 'International Calling'
-        feature2 = 'Hotspot'
-        feature3 = 'Metered Data'
-        feature4 = 'Unlimited Data'
-        self.assertEqual(phoneselection.feature(self.feature), feature1)
-        self.assertEqual(phoneselection.feature(self.feature), feature2)
-        self.assertEqual(phoneselection.feature(self.feature), feature3)
-        self.assertEqual(phoneselection.feature(self.feature), feature4)
+    #def test_feature(self, feature):
+        #self.feature = feature
+        #feature1 = 'International Calling'
+        #feature2 = 'Hotspot'
+        #feature3 = 'Metered Data'
+        #feature4 = 'Unlimited Data'
+        #self.assertEqual(phoneselection.feature(self.feature), feature1)
+        #self.assertEqual(phoneselection.feature(self.feature), feature2)
+        #self.assertEqual(phoneselection.feature(self.feature), feature3)
+        #self.assertEqual(phoneselection.feature(self.feature), feature4)
 
-class TestHandlingImproperCases(unittest.TestCase):
+#class TestHandlingImproperCases(unittest.TestCase):
 
     # Test Query 1: Test correctformat() to see if the numbers entered
     #               are in the correct format
-    def test_correctnumberformat(self, price):
-        self.price = price
-        self.assertIs(phoneselection.correctformat(self.price), float)
+    #def test_correctnumberformat(self, price):
+        #self.price = price
+        #self.assertIs(phoneselection.correctformat(self.price), float)
 
     # Test Query 2: Test validrange() to see if the plan price and
     #               features are within the correct range
-    def test_validrange(self, val_range):
-        self.val_range = val_range
-        for i in range(7.99, 160):
-            self.assertEqual(phoneselection.validrange(self.val_range), i)
+    #def test_validrange(self, val_range):
+        #self.val_range = val_range
+        #for i in range(7.99, 160):
+            #self.assertEqual(phoneselection.validrange(self.val_range), i)
 
     # Test Query 3: Test finalprice() to ensure the price equals
     #               the total of all inputs
-    def test_totalcost(self, phonePrice, featurePrice, priceOfplan, total):
-        self.phonePrice = phonePrice
-        self.featurePrice = featurePrice
-        self.priceOfplan = priceOfplan
-        self.total = total
+    #def test_totalcost(self, phonePrice, featurePrice, priceOfplan, total):
+        #self.phonePrice = phonePrice
+        #self.featurePrice = featurePrice
+        #self.priceOfplan = priceOfplan
+        #self.total = total
 
-        subtotal = self.phonePrice + self.featurePrice + self.priceOfplan
-        self.assertEqual(phoneselection.totalcost(self.total), subtotal)
+        #subtotal = self.phonePrice + self.featurePrice + self.priceOfplan
+        #self.assertEqual(phoneselection.totalcost(self.total), subtotal)
 
 
 # class TestTimeToAnswer(unittest.TestCase):
